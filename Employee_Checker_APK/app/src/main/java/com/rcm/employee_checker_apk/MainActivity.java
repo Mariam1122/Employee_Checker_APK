95f43867ca4d283325f919d6d7ce88079478d99a
Mariam Ali <93067663+Mariam1122@users.noreply.github.com>
2023-09-17 18:16:05 +0500

    Add files via upload

---
+++ Employee_Checker_APK/app/src/main/java/com/rcm/employee_checker_apk/MainActivity.java
@@ -0,0 +1,40 @@
+package com.rcm.employee_checker_apk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        view = findViewById(R.id.splash_screen);
+
+        /****** Create Thread that will sleep for 5 seconds****/
+        Thread background = new Thread() {
+            public void run() {
+                try {
+                    // Thread will sleep for 5 seconds
+                    sleep(5*1000);
+
+                    // After 5 seconds redirect to another intent
+                    Intent i=new Intent(getBaseContext(),First_screen.class);
+                    startActivity(i);
+
+                    //Remove activity
+                    finish();
+                } catch (Exception e) {
+                }
+            }
+        };
+        // start thread
+        background.start();
+    }
+}
